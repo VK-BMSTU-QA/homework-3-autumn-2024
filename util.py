2ba85b44b8cb52fb8467fcb264441e481d4b8452
Petr09Mitin <mpa22u514@student.bmstu.ru>
2024-11-26 12:45:23 +0300

    rm unused files

--- util.py
+++
@@ -1,7 +0,0 @@
-# Open the source file in read mode and the destination file in write mode
-with open('emails.csv', 'r') as source_file, open('output.csv', 'w') as destination_file:
-    # Read each line from the source file
-    for line in source_file:
-        new_line = "," + line.strip() + ",,,,,\n"
-        destination_file.write(new_line)
-    
